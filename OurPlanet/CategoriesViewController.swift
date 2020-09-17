4ef89c6943fbe1e7651cc63637c3034a18eca3d6
Tiago Do Couto <cs.couto@outlook.com>
2020-09-17 10:56:49 -0500

    completed chapter 10

--- OurPlanet/CategoriesViewController.swift
+++ OurPlanet/CategoriesViewController.swift
@@ -33,25 +33,77 @@ import RxCocoa
 class CategoriesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
   @IBOutlet var tableView: UITableView!
+  
+  let categories = BehaviorRelay<[EOCategory]>(value: [])
+  let disposeBag = DisposeBag()
 
   override func viewDidLoad() {
     super.viewDidLoad()
 
+    categories
+      .asObservable()
+      .subscribe(onNext: { [weak self] _ in
+        DispatchQueue.main.async {
+          self?.tableView.reloadData()
+        }
+      })
+      .disposed(by: disposeBag)
+    
     startDownload()
   }
 
   func startDownload() {
+    let eoCategories = EONET.categories
+    let downloadedEvents = eoCategories.flatMap { categories in
+      return Observable.from(categories.map { category in
+        EONET.events(for: 360, category: category)
+      })
+    }
+    .merge(maxConcurrent: 2)
     
+    let updatedCategories = eoCategories.flatMap { categories in
+      downloadedEvents.scan(categories) { updated, events in
+        return updated.map { category in
+          let eventsForCategory = EONET.filteredEvents(events: events, forCategory: category)
+          if !eventsForCategory.isEmpty {
+            var cat = category
+            cat.events = cat.events + eventsForCategory
+            return cat
+          }
+          return category
+        }
+      }
+    }
+
+    eoCategories
+      .concat(updatedCategories)
+      .bind(to: categories)
+      .disposed(by: disposeBag)
   }
   
   // MARK: UITableViewDataSource
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 0
+    return categories.value.count
   }
   
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: "categoryCell")!
+    let category = categories.value[indexPath.row]
+    cell.textLabel?.text = "\(category.name), events: \(category.events.count)"
+    cell.detailTextLabel?.text = category.description
+    cell.accessoryType = category.events.count > 0 ? .disclosureIndicator : .none
     return cell
+  }
+  
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    let category =  categories.value[indexPath.row]
+    tableView.deselectRow(at: indexPath, animated: true)
+    
+    guard !category.events.isEmpty else { return }
+    let eventsVC = storyboard!.instantiateViewController(withIdentifier: "events") as! EventsViewController
+    eventsVC.title = category.name
+    eventsVC.events.accept(category.events)
+    navigationController!.pushViewController(eventsVC, animated: true)
   }
   
 }
